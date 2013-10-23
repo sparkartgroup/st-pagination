3b0fb5de95d87b085427e057b1aa621fd3987129
Fauntleroy <the.kaempfer@gmail.com>
2013-10-23 12:35:36 -0700

    add preload test

--- test/assets/test.js
+++ test/assets/test.js
@@ -15964,6 +15964,7 @@ require('../jquery.st-pagination.js');
 require('./assets/jquery.mockjax.js');
 var test = require('tape');
 
+var AJAX_RESPONSETIME = 50;
 var TEST_MARKUP = [
 	'<div id="paginate-me"><ul class="st-items"><li>1</li><li>2</li><li>3</li></ul><a class="st-next" href="#next">Next</a></div>',
 	'<div id="paginate-me"><ul class="st-items"><li>4</li><li>5</li><li>6</li></ul><a class="st-next" href="#next">Next</a></div>',
@@ -15971,16 +15972,19 @@ var TEST_MARKUP = [
 	'<div id="paginate-me"><ul class="st-items"></ul><a class="st-next" href="#next">Next</a></div>'
 ];
 
-$.mockjax({
-	url: '/',
-	data: {
-		test: 1,
-		page: 1
-	},
-	status: 200,
-	responseText: '<!DOCTYPE html><html><body>'+ TEST_MARKUP[1] +'</body></html>',
-	responseTime: 5
-});
+for( var i in TEST_MARKUP ){
+	$.mockjax({
+		url: '/',
+		data: {
+			test: 1,
+			page: i + 1
+		},
+		status: 200,
+		dataType: 'html',
+		responseText: '<!DOCTYPE html><html><body>'+ TEST_MARKUP[i] +'</body></html>',
+		responseTime: AJAX_RESPONSETIME
+	});
+}
 
 test( 'stPagination method exists on $', function( t ){
 	t.plan(1);
@@ -15991,10 +15995,23 @@ test( 'stPagination initializes on element', function( t ){
 	t.plan(1);
 	$('#test').html( TEST_MARKUP[0] );
 	$('#paginate-me').stPagination({
-		url: '?test=1&page=:p'
+		url: '/?test=1&page=:p'
 	});
 	t.ok( $('#paginate-me').is('.initialized'), 'Adds initialized class' );
 	$('#test').html('');
+});
+
+test( 'stPagination preloads the next set of items', function( t ){
+	t.plan(1);
+	$('#test').html( TEST_MARKUP[0] );
+	$('#paginate-me').stPagination({
+		url: '/?test=1&page=:p'
+	});
+	setTimeout( function(){
+		var data = $('#paginate-me').data('st-pagination');
+		t.end();
+		$('#test').html('');
+	}, AJAX_RESPONSETIME + 1 );
 });
 },{"../jquery.st-pagination.js":1,"./assets/jquery.mockjax.js":31,"jquery":"MQ962V","tape":19}]},{},[32])
 ;
--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ require('../jquery.st-pagination.js');
 require('./assets/jquery.mockjax.js');
 var test = require('tape');
 
+var AJAX_RESPONSETIME = 50;
 var TEST_MARKUP = [
 	'<div id="paginate-me"><ul class="st-items"><li>1</li><li>2</li><li>3</li></ul><a class="st-next" href="#next">Next</a></div>',
 	'<div id="paginate-me"><ul class="st-items"><li>4</li><li>5</li><li>6</li></ul><a class="st-next" href="#next">Next</a></div>',
@@ -10,16 +11,19 @@ var TEST_MARKUP = [
 	'<div id="paginate-me"><ul class="st-items"></ul><a class="st-next" href="#next">Next</a></div>'
 ];
 
-$.mockjax({
-	url: '/',
-	data: {
-		test: 1,
-		page: 1
-	},
-	status: 200,
-	responseText: '<!DOCTYPE html><html><body>'+ TEST_MARKUP[1] +'</body></html>',
-	responseTime: 5
-});
+for( var i in TEST_MARKUP ){
+	$.mockjax({
+		url: '/',
+		data: {
+			test: 1,
+			page: i + 1
+		},
+		status: 200,
+		dataType: 'html',
+		responseText: '<!DOCTYPE html><html><body>'+ TEST_MARKUP[i] +'</body></html>',
+		responseTime: AJAX_RESPONSETIME
+	});
+}
 
 test( 'stPagination method exists on $', function( t ){
 	t.plan(1);
@@ -30,8 +34,21 @@ test( 'stPagination initializes on element', function( t ){
 	t.plan(1);
 	$('#test').html( TEST_MARKUP[0] );
 	$('#paginate-me').stPagination({
-		url: '?test=1&page=:p'
+		url: '/?test=1&page=:p'
 	});
 	t.ok( $('#paginate-me').is('.initialized'), 'Adds initialized class' );
 	$('#test').html('');
+});
+
+test( 'stPagination preloads the next set of items', function( t ){
+	t.plan(1);
+	$('#test').html( TEST_MARKUP[0] );
+	$('#paginate-me').stPagination({
+		url: '/?test=1&page=:p'
+	});
+	setTimeout( function(){
+		var data = $('#paginate-me').data('st-pagination');
+		t.end();
+		$('#test').html('');
+	}, AJAX_RESPONSETIME + 1 );
 });
